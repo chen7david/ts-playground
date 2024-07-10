bfb6392970002ad5a986ecf8ddd0a0c626bbba21
David Chen <chen7david@me.com>
2024-07-10 05:24:24 -0400

    example: one

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('hello world!')
+function dynamicPick<T extends object, K extends keyof T>(
+  obj: T,
+  keys: K[],
+): Pick<T, K> {
+  const result = {} as Pick<T, K>
+  keys.forEach((key) => {
+    if (key in obj) {
+      result[key] = obj[key]
+    }
+  })
+  return result
+}
+
+interface IUserEntity {
+  id: number
+  name: string
+  email: string
+  age: number
+}
+
+const user: IUserEntity = {
+  id: 1,
+  name: 'John Doe',
+  email: 'john.doe@example.com',
+  age: 30,
+}
+
+const userKeys = {
+  id: 1,
+  age: 30,
+}
+
+const compileTimeKeys = Object.keys(userKeys)
+
+const pickedUser = dynamicPick(user, ['name', 'email', 'age'])
+console.log(pickedUser) // Output: { name: 'John Doe', email: 'john.doe@example.com' }
